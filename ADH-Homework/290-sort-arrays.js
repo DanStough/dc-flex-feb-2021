131d65f034410031111319eec54c372323c79e64
Avery Hampton <averydhampton@icloud.com>
2021-03-16 16:01:06 -0500

    adds Homework folder to branch

---
+++ ADH-Homework/290-sort-arrays.js
@@ -0,0 +1,40 @@
+// TIP: check out this reference for how to sort arrays:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Write a function "alphaSort" that sorts an array of strings alphabetically.
+//
+// Examples:
+// alphaSort(['b', 'a', 'c'])
+// > ['a', 'b', 'c']
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Write a function "strLengthSort" that sorts an array of strings by how long
+// each string is. Put the shortest strings first.
+//
+// Examples:
+// strLengthSort(['Apple', 'Banana', 'Cherry'])
+// > ['Apple', 'Cherry', 'Banana']
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Write a function "sumSort". Given an array of array of numbers like:
+// var arr = [
+//   [1, 3, 4],
+//   [2, 4, 6, 8],
+//   [3, 6]
+// ];
+//
+// Sort the array by the sum of each inner array. For the above example, the
+// respective sums for each inner array is 8, 20, and 9.
+//
+// Example:
+// sumSort([
+//   [9, 1, 9],
+//   [2],
+//   [4, 5]
+// ])
+// > [[2], [4, 5], [9, 1, 9]]
