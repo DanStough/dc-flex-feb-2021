1b1d50aa7a287dedea5c11af161bd2d5c2dba28a
Robby Singh <kingraj2323@gmail.com>
2021-03-30 21:24:36 -0500

    Completed vanilla js stuff

--- js-vanilla-001/script.js
+++ js-vanilla-001/script.js
@@ -1,5 +1,224 @@
-// importing the stylesheet
-// import "./style.css";
+const countries = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "B",
+  "The Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "C",
+  "Cabo Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Congo, Democratic Republic of the",
+  "Congo, Republic of the",
+  "Costa Rica",
+  "Côte d'Ivoire",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "D",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "E",
+  "East Timor (Timor-Leste)",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "F",
+  "Fiji",
+  "Finland",
+  "France",
+  "G",
+  "Gabon",
+  "The Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "H",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "I",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "J",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "K",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Korea, North",
+  "Korea, South",
+  "Kosovo",
+  "Kuwait",
+  "Kyrgyzstan",
+  "L",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "M",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia, Federated States of",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Mozambique",
+  "Myanmar (Burma)",
+  "N",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Macedonia",
+  "Norway",
+  "O",
+  "Oman",
+  "P",
+  "Pakistan",
+  "Palau",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Q",
+  "Qatar",
+  "R",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "S",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Sudan, South",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "T",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "U",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Uruguay",
+  "Uzbekistan",
+  "V",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Y",
+  "Yemen",
+  "Z",
+  "Zambia",
+  "Zimbabwe"
+  ];
 
 // Begin the assignment
 const appDiv = document.getElementById('app');
@@ -38,14 +257,19 @@ containerDiv.appendChild(avatarDiv);
 const avatarPic = document.createElement('img');
 avatarPic.src = "https://picsum.photos/id/64/600/";
 avatarDiv.appendChild(avatarPic);
+
+const main = document.createElement('div');
+main.className = "main";
+containerDiv.appendChild(main);
+
 // Image within the container div
 const mainImage = document.createElement('img');
 mainImage.src = "https://picsum.photos/id/1015/320/568";
-containerDiv.appendChild(mainImage);
+main.appendChild(mainImage);
 // text-container class
 const textContainer = document.createElement('div');
 textContainer.className = "text-container";
-containerDiv.appendChild(textContainer);
+main.appendChild(textContainer);
 // H1 and p tags within text-container class
 const headerOne = document.createElement('h1');
 headerOne.innerText = "Norway is Calling";
@@ -62,3 +286,59 @@ containerDiv.appendChild(footerDiv);
 const footerParagraph = document.createElement('p');
 footerParagraph.innerText = "Private Policy";
 footerDiv.appendChild(footerParagraph);
+
+// button to change country name
+// const button = document.createElement('button');
+// button.innerText = "Change Country";
+// app.appendChild(button);
+
+let counter = 0;
+
+mainImage.addEventListener('click', function() {
+  headerOne.innerText = countries[counter];
+  console.log(counter);
+  counter++;
+})
+
+locations.addEventListener('click', function() {
+  const ul = document.createElement('ul');
+  ul.className = "countries";
+  main.innerText= "";
+  main.appendChild(ul);
+  for(let i = 0; i < countries.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = countries[i];
+    ul.appendChild(li);
+  }
+})
+
+pricing.addEventListener('click', function() {
+  const ul = document.createElement('ul');
+  ul.className = "countries";
+  main.innerText= "";
+  main.appendChild(ul);
+  for(let i = 0; i < countries.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = countries[i];
+    ul.appendChild(li);
+  }
+})
+
+contactUs.addEventListener('click', function() {
+  const ul = document.createElement('ul');
+  ul.className = "countries";
+  main.innerText= "";
+  main.appendChild(ul);
+  for(let i = 0; i < countries.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = countries[i];
+    ul.appendChild(li);
+  }
+})
+
+// written as anon function
+// const someFn = function() {
+//   alert('button clicked');
+// }
+
+// button.addEventListener('click', someFn);
