f508a60c658e333ef1509b660014e594447256a4
Avery Hampton <averydhampton@icloud.com>
2021-05-13 19:24:10 -0500

    adds some work to the WK-12  exercise

--- WK-12_T_Templates/exercise/index.js
+++ WK-12_T_Templates/exercise/index.js
@@ -1,16 +1,18 @@
+// 1. initialize express and templates **Done**
 
-// 1. initialize express and templates
+const data = require("./dataArray");
+console.log(data);
 
+app.get("/heartbeats", (req, res) => {
+  res.json({
+    is: "working",
+  });
+});
 
-const data = require('./dataArray')
-console.log(data)
-
-// 4. Detail page here. 
+// 4. Detail page here.
 
 // 5. List page here
 
-
 // 3. Write out a route to test your server is working
 
-
-// 2. Start your express server
+// 2. Start your express server **Done**
