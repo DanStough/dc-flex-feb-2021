e570afe43bc8bf4248834e3c8a0f5597648ff10e
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-05-24 22:07:33 -0500

    add authentication using github and passport

--- WK-13_S_OAuth-Passport/exercise/index.js
+++ WK-13_S_OAuth-Passport/exercise/index.js
@@ -1,13 +1,54 @@
-
+require('dotenv').config();
 const express = require("express");
+const session = require('express-session');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 const es6Renderer = require('express-es6-template-engine');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+const sess = {
+  secret:"nueral handshake",
+  cookie: {maxAge: 60000}
+}
+
 // 1. Add express middlewares for static files and body parsing
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(session(sess))
+app.use(express.static('public'));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:3001/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+  // console.log(JSON.stringify(profile))
+
+  // ASIDE: Access Tokens are super important! Treat them like passwords (never store in plain text)
+  // You can use this to talk to the Github API
+  // console.log("Access Token: " + accessToken)
+
+  // Tells passport to move on
+  cb(null, profile)
+  }
+));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(function(user, done) {
+  //What goes INTO the session here; right now it's everything in User
+  done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+  //This is looking up the User in the database using the information from the session "id"
+});
+
 
 // Configure Template Engine
 app.engine('html', es6Renderer);
@@ -16,8 +57,21 @@ app.set('view engine', 'html');
 
 const data = require('./dataObject')
 
+// ensures that the user is authenticated
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect("/")
+})
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+  res.redirect('/')
+}
+
 // 4. Add a post method to handle adding a new user
-app.post("/profile", (req,res) => {
+app.post("/profile", ensureAuthenticated, (req,res) => {
     console.log(req.body)
 
     // Create ID
@@ -31,7 +85,7 @@ app.post("/profile", (req,res) => {
     res.status(200).send()
 })
 
-app.get("/profile/:id", (req, res) => {
+app.get("/profile/:id", ensureAuthenticated, (req, res) => {
     const profile = data[req.params.id]
 
     if(!profile){
@@ -45,19 +99,36 @@ app.get("/profile/:id", (req, res) => {
     })
 })
 
-app.get("/", (req, res)=>{
-    const profileIds = Object.keys(data)
-    const profileArray = profileIds.map( id => data[id])
+app.get("/home", ensureAuthenticated, (req, res)=>{
+  const profileIds = Object.keys(data)
+  const profileArray = profileIds.map( id => data[id])
 
-    res.render('index', {
-        locals: {
-            profileArray
-        }
-    })
+  res.render('index', {
+      locals: {
+          profileArray
+      }
+  })
 })
 
-app.use(express.static('public'));
+app.get("/", (req, res)=>{
 
-app.listen(3000, ()=>{
-    console.log("running on port 3000")
+    res.send(`
+      <h1 class="text-center">Hello you must log in to access your account<h1>
+      <h1 class="text-center" ><a href="/auth/github">Login</a></h1>
+    `)
+})
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/home');
+  });
+
+
+app.listen(3001, ()=>{
+    console.log("running on port 3001")
 })
