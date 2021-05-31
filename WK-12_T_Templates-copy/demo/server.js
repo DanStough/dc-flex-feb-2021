9a6cfb81b2d5a4161abffe062d9cb1b6a1203c0d
Heggy Castaneda <heggyy@gmail.com>
2021-05-31 18:02:28 -0400

    working

--- WK-12_T_Templates-copy/demo/server.js
+++ WK-12_T_Templates-copy/demo/server.js
@@ -6,10 +6,23 @@ app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
+console.log('env var process.env !!!!!! ****** !!!!!', process.env);
+
+const name = "heggy";
+
 app.get("/home", (req, res) => {
+
+  // need data first
+  // fetch() or do a api call axios()
+  // axios()
+
+  // OR, need data from database
+  // db.get("data")
+
+
   res.render('home', {
     locals: {
-      
+      title: `${name}'s super nice pad`
     }
   });
 });
