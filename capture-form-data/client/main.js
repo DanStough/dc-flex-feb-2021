2bc8989fe1525d6c313a212d275f29b38f7bc712
Heggy Castaneda <heggyy@gmail.com>
2021-06-04 05:13:59 -0400

    response obj newcontact done

--- capture-form-data/client/main.js
+++ capture-form-data/client/main.js
@@ -7,26 +7,45 @@ form.addEventListener('submit', (e) => {
   // console.log('e.target: ', e.target);
   const data = new FormData(e.target);
   // console.log('data: ', data);
-  const stringified = stringifyFormData(data);
-  console.log('stringified:', stringified);
-  /**
-   * stringified: {
-      "name": "heggy",
-      "address": "111 front st",
-      "phone": "3333333",
-      "email": "k@g.com"
-     }
-   */
+  // we stringify data so that we could read it in the console
+  const formattedData = formatData(data);
+  // Example POST method implementation: 
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  // sendData(<endpoint str>, <data obj>)
+  sendData('http://localhost:8080/newcontact', formattedData);
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/API/FormData formData
-const stringifyFormData = fd => {
+const formatData = fd => {
   const data = {};
   // for...of loop just obj
   for (let key of fd.keys()) {
     data[key] = fd.get(key);
   }
-
-  // formData needs to be converted 
-  return JSON.stringify(data, null, 4);
+  return data;
 };
+
+// post method implementation https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
+// async/await doesn't block while waiting for response
+const sendData = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response;
+}
+
+// async function postData(url, data) {
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   });
+//   return response.json();
+// }
--- capture-form-data/server/server.js
+++ capture-form-data/server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // add express middleware for body parsing
 app.use(express.json()); // for parsing application/json
+app.use(cors()); // stop cors
 
 app.get('/heartbeat', (req, res) => {
   // res.send("I am here!");
@@ -11,15 +13,15 @@ app.get('/heartbeat', (req, res) => {
   });
 });
 
-// handle new user
+// handle post a new user
 app.post('/newcontact', (req, res) => {
-  console.log(req.body);
+  console.log('RESPONSE: ======>', req.body);
 
-  res.status(200).json({
+  res.json({
     "data received": req.body
   });
 });
 
 app.listen(8080, () => {
-  console.log("port running at 8080");
+  console.log("port running at http://localhost:8080");
 });
